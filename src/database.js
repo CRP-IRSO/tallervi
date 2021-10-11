0ef9e1322ef4ee9f5f9d208543efd610ce7bf402
CRP-IRSO <92340970+CRP-IRSO@users.noreply.github.com>
2021-10-11 20:39:21 -0300

    Update database.js

--- src/database.js
+++ src/database.js
@@ -3,19 +3,19 @@ const mysql = require('mysql');
 const mysqlConnection = mysql.createConnection({
 
     //Localmente
-    host: 'localhost',
-    user: 'root', //user por defecto
-    password: 'irso',
-    database: 'irso_2021',
-    multipleStatements: true
+    //host: 'localhost',
+    //user: 'root', //user por defecto
+    //password: 'irso',
+    //database: 'irso_2021',
+    //multipleStatements: true
 
     //En la web
-    // host: 'www.db4free.net',
-    // port: '3306',
-    // user: 'crpirso', //user por defecto
-    // password: 'bfef8ed1',
-    // database: 'irsotaller6',
-    // multipleStatements: true
+    host: 'www.db4free.net',
+    port: '3306',
+    user: 'crpirso', //user por defecto
+    password: 'bfef8ed1',
+    database: 'irsotaller6',
+    multipleStatements: true
 });
 
 //Verifico si me conecte correctamente a la base de datos

52f6d9a565825403297795b67fc0f1a9d01e9c85
ezequielnanton <ezequielnanton@hotmail.com>
2021-11-07 15:32:18 -0300

    add headres alumnos

--- src/routes/alumnos.js
+++ src/routes/alumnos.js
@@ -5,17 +5,7 @@ const validacion = require('../validaciones/validacion');
 const errores = require('../errores/errores');
 var _= require('lodash');
 
-app.all('*', function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'URLs to trust of allow');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    if ('OPTIONS' == req.method) {
-    res.sendStatus(200);
-    } else {
-      next();
-    }
-  });
-  
+
 // GET todos los alumnos
 router.get('/alumnos', (req, res) => {
 

a2cfc3d6de5cca13db3e0d206b9b5b5ee1d8ac0a
CRP-IRSO <92340970+CRP-IRSO@users.noreply.github.com>
2021-11-13 23:36:21 -0300

    Modificaron cant minimos

--- src/validaciones/validacion.js
+++ src/validaciones/validacion.js
@@ -20,10 +20,10 @@ module.exports = {
 
     validacionDatosPUT: (data) => {
         const schema = yup.object().shape({
-            id: yup.number().min(31).integer().nullable(true).typeError('id must be a number').positive('id must be greater than zero').required('id is required'),
-            nombres: yup.string().min(31).nullable(true).typeError('nombres must be a string').required('nombres is required'),
-            apellido: yup.string().min(31).nullable(true).typeError('apellido must be a string').required('apellido is required'),
-            direccion: yup.string().min(31).nullable(true).typeError('direccion must be a strig').required('direccion is required'),
+            id: yup.number().min(1).integer().nullable(true).typeError('id must be a number').positive('id must be greater than zero').required('id is required'),
+            nombres: yup.string().min(5).nullable(true).typeError('nombres must be a string').required('nombres is required'),
+            apellido: yup.string().min(5).nullable(true).typeError('apellido must be a string').required('apellido is required'),
+            direccion: yup.string().min(5).nullable(true).typeError('direccion must be a strig').required('direccion is required'),
             cod_postal: yup.string().min(4).nullable(true).typeError('cod_postal must be a string').required('cod_postal is required'),
             telefono: yup.number().min(8).integer().nullable(true).typeError('telefono must be a number').required('telefono is required'), 
         });

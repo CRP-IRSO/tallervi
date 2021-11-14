94a071c71cf1b9fb5d3c1f9633fad518c0c3f2f4
CRP-IRSO <92340970+CRP-IRSO@users.noreply.github.com>
2021-11-14 16:38:39 -0300

    Get todos los usuarios

--- src/routes/users.js
+++ src/routes/users.js
@@ -5,6 +5,64 @@ const validacion = require('../validaciones/validacion');
 const errores = require('../errores/errores');
 var _= require('lodash');
 
+// GET todos los users
+router.get('/users', (req, res) => {
+
+    console.log(req);
+    var sizeQuery = _.size(req.query)
+    var pathUrl = req.originalUrl;
+    var queryUrl = pathUrl.replace("/users?", "")
+    console.log("la query es: " + queryUrl)
+    var queryUrl = queryUrl.replace(/=/g, "='")
+    queryUrl = queryUrl.replace(/&/g, "' AND ")
+
+    console.log("primer: " + queryUrl)
+    queryUrl = queryUrl + "'"
+
+    console.log("segundo: " + queryUrl)
+
+    if (sizeQuery === 0) {
+        var sql = 'SELECT * FROM users';
+        connection.query(sql, (err, rows, fields) => {
+            if(!err) {
+                res.json({ users: rows });
+            } else {
+                console.log(err);
+            }
+        });
+
+    } else if(sizeQuery === 1) {
+        var sql = 'SELECT * FROM users WHERE ' + queryUrl ;
+        console.log("la query es: " + sql)
+        connection.query(sql, (err, rows, fields) => {
+            if(!err) {
+                res.json({ users: rows });
+            } else {
+                console.log(err);
+            }
+        });
+    } else if (sizeQuery === 2) {
+        var sql = 'SELECT * FROM users WHERE ' + queryUrl ;
+        console.log("la query es: " + sql)
+        console.log("el cod res: ")
+        console.log(res.statusCode)
+        connection.query(sql, (err, results, fields) => {
+
+            if(!err) {
+                res.json({ users: results });
+            } else {
+                console.log("error");
+                console.log(err.sqlMessage)
+                console.log(err.sqlState)
+                console.log("code")
+                console.log(err.code)
+                errores.error400(err, req, res)
+            }
+        });
+    }
+});
+
+// Insertar User
 router.post('/users', (req, res) => {
 
   //validacion.validacionDatosPOST(req.body);
@@ -23,6 +81,7 @@ router.post('/users', (req, res) => {
       }
   });
 
+// Modificar User
 router.put('/users/:id', (req, res) => {
 
       //validacion.validacionDatosPUT(req.body);

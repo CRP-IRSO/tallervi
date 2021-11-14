709b1954884f055d0d5cb7916d4cbf342e53f063
CRP-IRSO <92340970+CRP-IRSO@users.noreply.github.com>
2021-11-14 18:06:36 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 
 // Routes
 app.use(routerAlumnos);
-//app.use(routerUsers);
+app.use('/', require('./routes/users'));
 
 // Starting the server
 app.listen(app.get('port'), () => {

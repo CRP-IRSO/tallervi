b9accc3429151020620f087f9a80b4a5cca65d6a
CRP-IRSO <92340970+CRP-IRSO@users.noreply.github.com>
2021-11-13 23:34:18 -0300

    Cantidad minima para cod_postal y telefono

--- src/validaciones/validacion.js
+++ src/validaciones/validacion.js
@@ -24,8 +24,8 @@ module.exports = {
             nombres: yup.string().min(31).nullable(true).typeError('nombres must be a string').required('nombres is required'),
             apellido: yup.string().min(31).nullable(true).typeError('apellido must be a string').required('apellido is required'),
             direccion: yup.string().min(31).nullable(true).typeError('direccion must be a strig').required('direccion is required'),
-            cod_postal: yup.string().min(31).nullable(true).typeError('cod_postal must be a string').required('cod_postal is required'),
-            telefono: yup.number().min(31).integer().nullable(true).typeError('telefono must be a number').required('telefono is required'), 
+            cod_postal: yup.string().min(4).nullable(true).typeError('cod_postal must be a string').required('cod_postal is required'),
+            telefono: yup.number().min(8).integer().nullable(true).typeError('telefono must be a number').required('telefono is required'), 
         });
         schema.validateSync(data);
     }

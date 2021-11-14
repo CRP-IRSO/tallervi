c4c17c0cba93002fac35a841c79129c9afcfcfab
CRP-IRSO <92340970+CRP-IRSO@users.noreply.github.com>
2021-11-14 00:45:04 -0300

    ocultar Id

--- src/validaciones/validacion.js
+++ src/validaciones/validacion.js
@@ -21,7 +21,7 @@ module.exports = {
 
     validacionDatosPUT: (data) => {
         const schema = yup.object().shape({
-            id: yup.number().min(1).integer().nullable(true).typeError('id must be a number').positive('id must be greater than zero').required('id is required'),
+            //id: yup.number().min(1).integer().nullable(true).typeError('id must be a number').positive('id must be greater than zero').required('id is required'),
             nombres: yup.string(true).min(5).nullable(true).typeError('nombres must be a string').required('nombres is required'),
             apellido: yup.string().min(5).nullable(true).typeError('apellido must be a string').required('apellido is required'),
             direccion: yup.string().min(5).nullable(true).typeError('direccion must be a strig').required('direccion is required'),

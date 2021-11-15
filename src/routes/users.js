9d011eb1e07ef5ecce2d167f7175e4b17906bb3c
CRP-IRSO <chroblesp@gmail.com>
2021-11-14 23:33:50 -0300

    Update users.js

--- src/routes/users.js
+++ src/routes/users.js
@@ -82,7 +82,7 @@ var _= require('lodash');
 // Autenticación
 router.post('/auth',(req, res) =>{
   const user = req.body.user;
-  const pass = req.body.user;
+  const pass = req.body.pass;
   if(user && pass){
     connection.query('SELLECT * FROM users WHERE user = ?', [user], (err, rows, fields) => {
       if(results.length == 0){

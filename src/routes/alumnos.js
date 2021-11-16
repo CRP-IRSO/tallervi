22a10d4ac58c69d503356622dd328407d8c9ef2a
CRP-IRSO <chroblesp@gmail.com>
2021-11-15 23:33:15 -0300

    Update alumnos.js

--- src/routes/alumnos.js
+++ src/routes/alumnos.js
@@ -117,7 +117,7 @@ router.post('/alumnos', (req, res) => {
     connection.query(sql, [nombres , apellido, direccion, cod_postal, telefono], (err, rows, fields) => {
         if(!err) {
             res.status(201);
-            res.json({status: "Alumno Insertado"});
+            res.json({"Id insertado:"{id}});
         } else {
             console.log(err);
         }

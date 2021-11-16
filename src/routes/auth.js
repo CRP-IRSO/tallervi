a431b438991d6959ff685a3cd6afb76d9224cd5e
CRP-IRSO <chroblesp@gmail.com>
2021-11-16 18:50:18 -0300

    routes auth

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -6,7 +6,7 @@ const errores = require('../errores/errores');
 var _= require('lodash');
 
 // Autenticación
-/*router.post('/auth',(req, res) =>{
+router.post('/auth',(req, res) =>{
 
   const user = req.body.user;
   const pass = req.body.pass;
@@ -20,7 +20,7 @@ var _= require('lodash');
       }
     })
   }
-});*/
+});
 
 //router.get('*', (req, res) => { errores.error404(req, res) });
 router.use((error, req, res, next) =>{errores.error400(error, req, res, next)});
